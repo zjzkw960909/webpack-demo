bc50c21e4f50b2ccb0f07bc8ac2887aefe5a2fc6
System Administrator <root@kaiwendeMacBook-Pro.local>
2017-05-24 00:04:46 +0800

    fix

---
+++ app/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="app">
+    <div class="example">{{ msg }}</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
+
+<style>
+.example {
+  color: red;
+}
+</style>
